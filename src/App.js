c1282b8f454ddf38ebd23e6458c9f80e8308c463
Corbin Bourassa <91101975+CorbinB24@users.noreply.github.com>
2021-11-22 20:16:50 -0500

    added second component page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-import Header from './components/headerComponent';
+import React, { Component } from "react";
+import Header from "./components/headerComponent";
+import Main from "./components/mainComponent";
+
+
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-              <Header />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/mainComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+
+
+
+class Main extends React.Component {
+    render() { 
+        return(
+            <div className='container'>
+                <div className='row'>
+                    <div className='col'>
+                        <h1>Hello World</h1>
+                    </div>
+                </div>
+            </div>
+        ); 
+    };
+}
+ 
+export default Main;
